6eb9671b10af3750e533fe1e212c7b5281755ca2
surajverma2587 <surajverma2587@gmail.com>
2022-03-01 20:23:18 +0000

    add form validation

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,26 @@
+import { Link as RouterLink } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
-import { Link as RouterLink } from "react-router-dom";
+import Typography from "@mui/material/Typography";
 
 export const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("form submitted", data);
+  };
+
+  const validateForm = (formErrors) => {
+    return !!formErrors.email || !!formErrors.password;
+  };
+
   return (
     <Box
       component="form"
@@ -14,6 +30,7 @@ export const LoginForm = () => {
         flexDirection: "column",
         alignItems: "center",
       }}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <TextField
         margin="normal"
@@ -22,6 +39,8 @@ export const LoginForm = () => {
         name="email"
         variant="outlined"
         fullWidth
+        {...register("email", { required: true })}
+        error={!!errors.email}
       />
       <TextField
         type="password"
@@ -31,6 +50,8 @@ export const LoginForm = () => {
         name="password"
         variant="outlined"
         fullWidth
+        {...register("password", { required: true })}
+        error={!!errors.password}
       />
       <Button type="submit" variant="contained" fullWidth sx={{ mt: 3, mb: 2 }}>
         Login
@@ -43,6 +64,16 @@ export const LoginForm = () => {
       >
         Don't have an account? Sign Up
       </Link>
+      {validateForm(errors) && (
+        <Typography
+          variant="subtitle2"
+          gutterBottom
+          component="div"
+          sx={{ marginTop: "2rem", color: "#d32f2f", textAlign: "center" }}
+        >
+          Failed to login, please enter valid email address and/or password.
+        </Typography>
+      )}
     </Box>
   );
 };
