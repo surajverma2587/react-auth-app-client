c00a257c9be6c3808b044dc2a33ba17e357b0454
surajverma2587 <surajverma2587@gmail.com>
2022-03-01 20:32:57 +0000

    connect apollo client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import { BrowserRouter } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+  setContext,
+} from "@apollo/client";
+
 import { NavBar } from "./components/NavBar";
 import { AppRouter } from "./components/AppRouter";
 
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+  credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 export const App = () => {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <AppRouter />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <NavBar />
+        <AppRouter />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 };
