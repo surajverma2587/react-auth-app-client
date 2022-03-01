711b5eb56e97494a9904e725d82cf06aaba8c07c
surajverma2587 <surajverma2587@gmail.com>
2022-03-01 19:34:32 +0000

    form design using mui

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,48 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import { Link as RouterLink } from "react-router-dom";
 
 export const LoginForm = () => {
-  return <Button variant="contained">Hello World</Button>;
+  return (
+    <Box
+      component="form"
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <TextField
+        margin="normal"
+        id="email"
+        label="Email"
+        name="email"
+        variant="outlined"
+        fullWidth
+      />
+      <TextField
+        type="password"
+        margin="normal"
+        id="password"
+        label="Password"
+        name="password"
+        variant="outlined"
+        fullWidth
+      />
+      <Button type="submit" variant="contained" fullWidth sx={{ mt: 3, mb: 2 }}>
+        Login
+      </Button>
+      <Link
+        component={RouterLink}
+        to="/sign-up"
+        variant="body2"
+        underline="none"
+      >
+        Don't have an account? Sign Up
+      </Link>
+    </Box>
+  );
 };
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,11 @@
+import Container from "@mui/material/Container";
+
 import { LoginForm } from "../components/LoginForm";
 
 export const LoginPage = () => {
   return (
-    <div>
+    <Container component="main" maxWidth="xs">
       <LoginForm />
-    </div>
+    </Container>
   );
 };
